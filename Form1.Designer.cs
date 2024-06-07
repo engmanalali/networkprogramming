cbdae9abdd55a1e040ffb9199da2ae326ccc2ac8
Manal <126725104+engmanalali@users.noreply.github.com>
2024-06-07 11:41:45 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace np_image_manual_server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListenButton = new System.Windows.Forms.Button();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.ImageShowBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageShowBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ListenButton
+            // 
+            this.ListenButton.Location = new System.Drawing.Point(870, 40);
+            this.ListenButton.Name = "ListenButton";
+            this.ListenButton.Size = new System.Drawing.Size(198, 97);
+            this.ListenButton.TabIndex = 0;
+            this.ListenButton.Text = "Listen";
+            this.ListenButton.UseVisualStyleBackColor = true;
+            this.ListenButton.Click += new System.EventHandler(this.ListenButton_Click);
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Location = new System.Drawing.Point(870, 205);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(198, 97);
+            this.BrowseButton.TabIndex = 1;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(870, 367);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(198, 97);
+            this.SendButton.TabIndex = 2;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // ImageShowBox
+            // 
+            this.ImageShowBox.Location = new System.Drawing.Point(29, 40);
+            this.ImageShowBox.Name = "ImageShowBox";
+            this.ImageShowBox.Size = new System.Drawing.Size(812, 424);
+            this.ImageShowBox.TabIndex = 3;
+            this.ImageShowBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(1108, 506);
+            this.Controls.Add(this.ImageShowBox);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.ListenButton);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ImageShowBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ListenButton;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.PictureBox ImageShowBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace np_image_manual_server
+{
+    public partial class Form1 : Form
+    {
+        // Normal steps.
+        Socket server;
+        Socket client;
+        NetworkStream ns;
+        StreamReader sr;
+        StreamWriter sw;
+
+        // The path of our image.
+        private string selectedImagePath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void ListenButton_Click(object sender, EventArgs e)
+        {
+            // Normal steps.
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(ipep);
+            server.Listen(5);
+            client = await Task.Run(() => server.Accept());
+            ns = new NetworkStream(client);
+            sr = new StreamReader(ns);
+            sw = new StreamWriter(ns);
+
+            // To make our server always be able to recieve an image.
+            while (true)
+            {
+                try
+                {
+                    //***********************************************
+                    // NOTE:                                        *
+                    // Detailed comments are in the client side.    *
+                    //***********************************************
+
+                    byte[] sizeData = new byte[4];
+                    await ns.ReadAsync(sizeData, 0, sizeData.Length);
+                    int imageSize = BitConverter.ToInt32(sizeData, 0);
+                    byte[] imageData = new byte[imageSize];
+                    int totalBytesRead = 0;
+
+                    // Loop until all image data are read.
+                    while (totalBytesRead < imageSize)
+                    {
+                        int bytesRead = await ns.ReadAsync(imageData, totalBytesRead, imageSize - totalBytesRead);
+                        totalBytesRead += bytesRead;
+                    }
+
+                    ImageShowBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    // Display the image in the PictureBox
+                    using (MemoryStream memoryStream = new MemoryStream(imageData))
+                    {
+                        ImageShowBox.Image = Image.FromStream(memoryStream);
+                    }
+
+                    // Just for debugging.
+                    // MessageBox.Show("Image received and displayed successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred while receiving and displaying the image: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            //***********************************************
+            // NOTE:                                        *
+            // Detailed comments are in the client side.    *
+            //***********************************************
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg;*.jpeg;*.png;*.gif)|*.jpg;*.jpeg;*.png;*.gif|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedImagePath = openFileDialog.FileName;
+                ImageShowBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                ImageShowBox.Image = new System.Drawing.Bitmap(openFileDialog.FileName, false);
+            }
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            //***********************************************
+            // NOTE:                                        *
+            // Detailed comments are in the client side.    *
+            //***********************************************
+
+            if (string.IsNullOrEmpty(selectedImagePath))
+            {
+                MessageBox.Show("Please select an image first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                byte[] imageData = File.ReadAllBytes(selectedImagePath);
+                byte[] sizeData = BitConverter.GetBytes(imageData.Length);
+
+                ns.Write(sizeData, 0, sizeData.Length);
+                ns.Flush();
+
+                ns.Write(imageData, 0, imageData.Length);
+                ns.Flush();
+
+                // For debugging.
+                // MessageBox.Show("Image sent successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
